50178b887e6cc55c05aa059682ebe75054e0dfa1
fnaranjo <fnaranjo@fnaranjo>
2019-06-28 17:02:20 -0500

    init

---
+++ asperacos/src/main/java/asperacos/asperacos/FileUploader.java
@@ -0,0 +1,17 @@
+package asperacos.asperacos;
+
+/**
+ * Hello world!
+ *
+ */
+public class FileUploader 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+    
+    
+    
+    
+}
